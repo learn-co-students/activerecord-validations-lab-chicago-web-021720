c57d77512c4f3c75c6c18a98c2b35477011926b2
djoverbeatz29 <jroverby92@gmail.com>
2020-03-18 11:17:39 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+
+    validates_presence_of :title
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {
+        in: ["Fiction", "Non-Fiction"],
+        message: "%{value} must be Fiction or Non-Fiction"}
+    
+    validate :clickbaity
+
+    def clickbaity
+        if title
+            if ![/Won\'t Believe/, /Secret/, /Top [0-9]+/, /Guess/].find { |reg| title.match(reg) }
+                errors.add(:title, "is not clickbaity enuf. Try again!")
+            end
+        end
+    end
+
 end
